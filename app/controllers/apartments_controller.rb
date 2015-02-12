2db9edc829b2feba493b4a87889d1d6ddda84be4
Vadim Gribanov <v1m@tut.by>
2015-02-13 01:17:27 +0300

    Add timeseries chart

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -1,5 +1,17 @@
 class ApartmentsController < ApplicationController
   def index
-    @apartments = Apartment.group(:apart_type).average(:price_usd).sort(:apart_type)
+    @apartments = Apartment.group(:apart_type).order(:apart_type).median(:price_usd)
+    puts @apartments.inspect
+
+    @apartments_by_day = Apartment
+      .group('date_trunc(\'day\',created_at)','apart_type')
+      .order('date_trunc(\'day\',created_at)')
+      .median(:price_usd)
+      .reduce({}) { |res, apartment|
+        res[apartment[0][1]] = res[apartment[0][1]].nil? ? {apartment[0][0] => apartment[1]} : res[apartment[0][1]][apartment[0][0]] = apartment[1]
+        res
+      }.flat_map {|z,a| [name: z, data: a]}
+
+    puts @apartments_by_day.inspect
   end
 end
