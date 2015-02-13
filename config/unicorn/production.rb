82ab8da4c55c9f24ad2413b7626791ad3eeadfdf
Vadim Gribanov <v1m@tut.by>
2015-02-13 14:19:49 +0300

    test

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,5 +1,5 @@
-app_dir = File.expand_path('../../', __FILE__)
-shared_dir = File.expand_path('../../../shared/', __FILE__)
+app_dir = File.expand_path('../../../', __FILE__)
+shared_dir = File.expand_path('../../../../shared/', __FILE__)
 # Set unicorn options
 worker_processes 2
 preload_app true
@@ -9,14 +9,14 @@ timeout 30
 working_directory app_dir
 
 # Set up socket location
-listen "#{shared_dir}/sockets/unicorn.sock", :backlog => 64
+listen "#{shared_dir}/tmp/sockets/unicorn.sock", :backlog => 64
 
 # Loging
 stderr_path "#{shared_dir}/log/unicorn.stderr.log"
 stdout_path "#{shared_dir}/log/unicorn.stdout.log"
 
 # Set master PID location
-pid "#{shared_dir}/pids/unicorn.pid"
+pid "#{shared_dir}/tmp/pids/unicorn.pid"
 
 before_fork do |server, worker|
   defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
